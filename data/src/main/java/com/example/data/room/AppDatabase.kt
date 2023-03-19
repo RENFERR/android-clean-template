8099936231f0166f3f4b3910330ee16d1adab1cd
Маенков Кирилл <maenkov00@mail.ru>
2023-03-19 12:11:27 +0400

    refactor data logic

--- data/src/main/java/com/example/data/room/AppDatabase.kt
+++ data/src/main/java/com/example/data/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.data.room.dao.ContentDao
-import com.example.data.storage.models.Model
+import com.example.data.storage.models.entities.Model
 
 @Database(entities = [Model::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
